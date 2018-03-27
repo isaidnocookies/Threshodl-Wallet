e5109023fe2a00dc2734348afe105a6cc2e5a7a8
Joseph Cohen <joseph@threebx.com>
2018-03-26 17:14:21 -0700

    Reply to create address added

--- Server/restalphamanageaddress.cpp
+++ Server/restalphamanageaddress.cpp
@@ -13,7 +13,20 @@ bool RESTAlphaManageAddress::serviceCreate(const QByteArray &iRESTArguments, Htt
         QJsonParseError lJsonError;
         QVariantMap     lMap = QJsonDocument::fromJson(iRequest.getBody(), &lJsonError).toVariant().toMap();
         if( lJsonError.error == QJsonParseError::NoError ) {
-            if( lMap.contains(QStringLiteral("address")) && lMap.contains("publicKey") ) {
+            if( lMap.contains(QStringLiteral("address")) && lMap.contains(QStringLiteral("publicKey")) ) {
+
+                QVariantMap lResponse;
+                lResponse[QStringLiteral("created")] = 0;
+
+                if( gApp->databaseInterface()->addressCreate(lMap[QStringLiteral("address")].toString(), lMap[QStringLiteral("publicKey")].toByteArray()) ) {
+                    lResponse[QStringLiteral("created")] = 1;
+                } else {
+                    qInfo() << "Failed to create account for" << lMap[QStringLiteral("address")].toString();
+                }
+
+                iResponse.setHeader("Content-Type", "application/json");
+                iResponse.write( QJsonDocument::fromVariant(lResponse).toJson() );
+                return true;
 
             } else {
                 qWarning() << "Received an invalid create address request, corrupt message that is missing one or more invalid JSON fields.";
