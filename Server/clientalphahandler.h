9f297e8ada4a990b25e555b4ae515f4829d1ff53
Joseph Cohen <joseph@threebx.com>
2018-04-10 10:39:04 -0700

    Added authenticate message type

--- Server/clientalphahandler.h
+++ Server/clientalphahandler.h
@@ -7,7 +7,9 @@
 class ClientAlphaHandler
 {
 protected:
+    static bool authenticateMessage(RPCMessage &iMessage);
     static bool createUserAccount(ClientConnection * iConnection, RPCMessage &iMessage);
+    static bool createMicroWalletPackage(ClientConnection * iConnection, RPCMessage &iMessage);
 
 public:
     static bool handle(ClientConnection * iConnection, const QString iCommand, RPCMessage &iMessage);
