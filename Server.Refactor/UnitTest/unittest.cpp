7d6f2e4600b8de1858dc714ae52e481a8bca484c
Joseph Cohen <joseph@threebx.com>
2018-05-05 01:06:01 -0700

    Lots of unit test work

---
+++ Server.Refactor/UnitTest/unittest.cpp
@@ -0,0 +1,10 @@
+#include "../config.h"
+
+void UnitTestInjectCommandConfiguration(Config * iConfiguration)
+{
+    Q_UNUSED(iConfiguration)
+
+    // Do nothing for now, later we can inject custom configuration settings and filters here
+
+    return;
+}
