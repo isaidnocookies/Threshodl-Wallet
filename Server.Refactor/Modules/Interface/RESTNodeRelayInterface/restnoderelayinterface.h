a4d5ce8bce1005fab7706f170c3dc4eb83586e4b
Joseph Cohen <joseph@threebx.com>
2018-05-19 11:25:01 -0700

    New RESTNodeRelayInterface module

---
+++ Server.Refactor/Modules/Interface/RESTNodeRelayInterface/restnoderelayinterface.h
@@ -0,0 +1,13 @@
+#ifndef RESTNODERELAYINTERFACE_H
+#define RESTNODERELAYINTERFACE_H
+
+class RESTNodeRelayInterfaceInterface
+{
+public:
+    virtual ~RESTNodeRelayInterfaceInterface()
+    { }
+
+    virtual void an_example_method() = 0;
+};
+
+#endif // RESTNODERELAYINTERFACE_H
