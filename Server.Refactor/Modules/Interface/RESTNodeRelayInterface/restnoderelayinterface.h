5eca8abb05865577efe27915f680779a1ca99f57
Joseph Cohen <joseph@threebx.com>
2018-05-19 11:34:44 -0700

    Added virtual interfaces to RESTNodeRelayInterface that should follow the connection to how the module will work

--- Server.Refactor/Modules/Interface/RESTNodeRelayInterface/restnoderelayinterface.h
+++ Server.Refactor/Modules/Interface/RESTNodeRelayInterface/restnoderelayinterface.h
@@ -10,7 +10,8 @@ public:
     virtual ~RESTNodeRelayInterfaceInterface()
     { }
 
-    virtual QNetworkReply * relayAndWaitForResponse(QNetworkRequest &iRequest, int iTimeOutInMS = 30000) = 0;
+    virtual QNetworkReply * get(const QString iCryptoTypeShortName, const QString iUrlPath)                                                         = 0;
+    virtual QNetworkReply * post(const QString iCryptoTypeShortName, const QString iUrlPath, const QString iPayloadType, const QByteArray iPayload) = 0;
 };
 
 #endif // RESTNODERELAYINTERFACE_H
